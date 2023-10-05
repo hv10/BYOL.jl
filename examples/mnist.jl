6133d4e84093294f930ddf8a3fcb72a33083a072
hv10 <noeldanz@gmail.com>
2023-10-05 10:08:15 +0200

    update example

--- examples/mnist.jl
+++ examples/mnist.jl
@@ -6,17 +6,17 @@ using Images
 using Optimisers
 using ProgressBars
 using Flux
+using StatsPlots
+using Random
+using Distributions
 using BYOL
 
 
 main() = begin
     # Load the MNIST dataset
     train = MNIST(split=:train)
-    test = MNIST(split=:test)
     X_train = train.features
     y_train = train.targets
-    X_test = test.features
-    y_test = test.targets
 
 
     # define the augmentations used to differentiate samples
@@ -32,10 +32,9 @@ main() = begin
         MLUtils.flatten,
         Dense(28^2, 8, relu),
         Dense(8, 16, relu),
-        Dense(16, 10),
-        softmax
+        Dense(16, 3)
     )
-    projector, predictor = make_mlp(10 => 16), make_mlp(16 => 16)
+    projector, predictor = make_mlp(3 => 8), make_mlp(8 => 8)
     online = WrappedNetwork(model, projector, predictor)
     target = deepcopy(online)
     opt_state = Optimisers.setup(Optimisers.Adam(), online)
@@ -43,16 +42,32 @@ main() = begin
     aug(x) = mapslices(x -> augment(x, pl), x, dims=(1, 2))
 
 
-    train_dl = MLUtils.DataLoader((X_train, y_train), batchsize=2, shuffle=true, collate=true)
+    train_dl = MLUtils.DataLoader((X_train, y_train), batchsize=256, shuffle=true, collate=true)
 
+    figs = []
     for i in 1:20
         @info "Epoch $i"
-        for (x, y) in ProgressBar(train_dl)
-            @show size(online(aug(x))[3])
+        for (x, _) in ProgressBar(train_dl)
             opt_state, online, target = byol_update!(opt_state, x, online, target, aug, aug)
-            break
         end
-        break
+        push!(figs, plot_mnist_emb_space(online.net))
+        display(figs[end])
     end
-    return online
+    @gif for fig in figs
+        fig
+    end fps = 4
+    return online, figs
+end
+
+plot_mnist_emb_space(model) = begin
+    test = MNIST(split=:test)
+    idxs = sample(axes(test.features, 3), 100)
+    X_test = test.features[:, :, idxs]
+    y_test = test.targets[idxs]
+    yhat = model(X_test)
+    scatter(yhat[1, :], yhat[2, :], yhat[3, :], color=y_test, legend=false)
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
 end
